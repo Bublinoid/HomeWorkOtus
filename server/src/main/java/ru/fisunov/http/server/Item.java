7526e4a14e07de794740681c6e51aa8cf735b530
Hleb <gleb.qb@gmail.com>
2024-01-11 13:58:59 +0100

    web application2 is ready

--- server/src/main/java/ru/fisunov/http/server/Item.java
+++ server/src/main/java/ru/fisunov/http/server/Item.java
@@ -1,30 +1,34 @@
-package ru.fisunov.http.server;
+    package ru.fisunov.http.server;
 
-public class Item {
-    private Long id;
-    private String title;
+    import com.google.gson.annotations.SerializedName;
 
-    public Long getId() {
-        return id;
+    public class Item {
+    @SerializedName("id")
+        private Long id;
+    @SerializedName("title")
+        private String title;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public Item() {
+        }
+
+        public Item(Long id, String title) {
+            this.id = id;
+            this.title = title;
+        }
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Item() {
-    }
-
-    public Item(Long id, String title) {
-        this.id = id;
-        this.title = title;
-    }
-}
